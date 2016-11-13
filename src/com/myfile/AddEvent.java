6306008dca7e1cd35ecc8d52630a0f98c2dd71ec
vachester <xcchester@gmail.com>
2016-11-13 19:53:09 +0800

    test

--- src/com/myfile/AddEvent.java
+++ src/com/myfile/AddEvent.java
@@ -8,6 +8,7 @@ import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -57,7 +58,7 @@ public class AddEvent extends ActionSupport implements ServletRequestAware {
 //		}
 //		String data = "饮食&开心&随便&30&2016-11-13&HIT&I/TEST&test2&HIT/Fuck&2001-1-1&2001-1-1&3&4";
 		try {
-			String sqlcmd, tableName, value, PDOName;
+			String sqlcmd, tableName, PDOName;
 			Timestamp t = new Timestamp(new Date().getTime());
 			ResultSet mydata;
 			String[] events = data.split("\\/");
@@ -65,11 +66,20 @@ public class AddEvent extends ActionSupport implements ServletRequestAware {
 			ResultSetMetaData column;
 			HashMap<String, Integer> linkdata = null;
 			linkdata = new HashMap<>();
+			int count = 0;
 			for (String event : events) {
 				String[] detail = event.split("\\&");
 				PDOName = detail[0];
 				tableName = userName + "_" + PDOName;
 				ArrayList<String> header = new ArrayList<String>();
+				sqlcmd = "select * from pdos where userName=? and PDOName=?";
+				mysql = new SQLManage(sqlcmd);
+				mysql.setString(1, userName);
+				mysql.setString(2, PDOName);
+				mydata = mysql.executeQuery();
+				if(mydata.next()) {
+					count = mydata.getInt("counts");
+				}
 				sqlcmd = "select * from " + userName + "_" + PDOName;
 				mysql = new SQLManage(sqlcmd);
 				mydata = mysql.executeQuery();
@@ -100,6 +110,12 @@ public class AddEvent extends ActionSupport implements ServletRequestAware {
 				if (mydata.next()) {
 					linkdata.put(PDOName, mydata.getInt("max(eventID)"));
 				}
+				sqlcmd = "update pdos set counts=? where userName=? and PDOName=?";
+				mysql = new SQLManage(sqlcmd);
+				mysql.setInteger(1, count+1);
+				mysql.setString(2, userName);
+				mysql.setString(3, PDOName);
+				mysql.executeUpdate();
 			}
 			String linkformat;
 			for (String pdo1 : linkdata.keySet()) {
---
+++ src/com/search/Search.java
@@ -0,0 +1,116 @@
+package com.search;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.struts2.ServletActionContext;
+import org.apache.struts2.interceptor.ServletRequestAware;
+
+import com.opensymphony.xwork2.ActionSupport;
+import com.sql.SQLManage;
+
+public class Search extends ActionSupport implements ServletRequestAware {
+	private String status;
+	private HttpServletRequest request;
+//	private String choice;
+	private ArrayList<HashMap<String, String>> results = new ArrayList<>();
+	
+	public ArrayList<HashMap<String, String>> getResults() {
+		return results;
+	}
+
+	public void setResults(ArrayList<HashMap<String, String>> results) {
+		this.results = results;
+	}
+
+//	public String getChoice() {
+//		return choice;
+//	}
+//
+//	public void setChoice(String choice) {
+//		this.choice = choice;
+//	}
+
+	public HttpServletRequest getServletRequest() {
+		return request;
+	}
+
+	public void setServletRequest(HttpServletRequest request) {
+		this.request = request;
+	}
+	
+	public boolean belongTo(String s1, String s2) {
+		boolean judge = true;
+		for(int i = 0;i < s1.length();i++) {
+			if(s1.charAt(i) == '1') {
+				if(s2.charAt(i) != '1') {
+					judge = false;
+					break;
+				}
+			}
+		}
+		return judge;
+	}
+	
+	public String searchResult() {
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpSession session = req.getSession();
+		HttpServletResponse response = ServletActionContext.getResponse(); 
+		response.setContentType("application/msexcel;charset=UTF-8"); 
+		String userName = session.getAttribute("userName").toString();
+		//前台编码格式为111/*&*/*/*
+		String choice = "101/as&1/ /哈工大";
+		try {
+			String sqlcmd;
+			SQLManage mysql;
+			ResultSet mydata;
+			String[] parse = choice.split("\\/");
+			HashMap<String , String> pdos = new HashMap<>();
+			sqlcmd="select * from pdos where userName=?";
+			mysql = new SQLManage(sqlcmd);
+			mysql.setString(1, userName);
+			mydata = mysql.executeQuery();
+			while(mydata.next()) {
+				pdos.put(mydata.getString("PDOName"), mydata.getString("tag"));
+			}
+			
+			//开始遍历各个pdo
+			for(String PDOName : pdos.keySet()) {
+				if(belongTo(parse[0], pdos.get(PDOName))) {
+					sqlcmd = "select * from " + userName+"_"+PDOName;
+					if(choice.charAt(1) == '1') {
+						sqlcmd = " 地点=?";
+						if(choice.charAt(2) == '1') {
+							sqlcmd = " and 人物=?";
+						}
+					}
+					else if(choice.charAt(2) == '1') {
+						sqlcmd = " 人物=?";
+					}
+					mysql = new SQLManage(sqlcmd);
+					for(int i = 2;i < parse.length; i++) {
+						if(choice.charAt(i-1) == '1') {
+							mysql.setString(i-1, parse[i]);
+						}
+					}
+					System.out.println(sqlcmd);
+				}
+			}
+			status = SUCCESS;
+			mysql.close();
+		} catch (ClassNotFoundException e) {
+			status = ERROR;
+			e.printStackTrace();
+		} catch (SQLException e) {
+			status = ERROR;
+			e.printStackTrace();
+		}
+		return status;
+	}
+}
